cb37955e2d5806e82ffd01736d761bf9741ceb84
kodyamani <40610898+kodyamani@users.noreply.github.com>
2018-06-27 07:00:02 -0700

    Add files via upload

---
+++ Dark Robot/Dark_Robot.ino
@@ -0,0 +1,20 @@
+const int photoResistor = A0;
+const int threshold = 311;
+
+int sensorReading = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorReading = analogRead(photoResistor);
+  Serial.println(sensorReading);
+
+  if(sensorReading <= 311) {
+    Serial.println("Ahhh! It's dark!");
+    delay(2000);
+  }
+  delay(100);
+}
+
